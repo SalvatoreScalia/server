9817de48909482f2056a038358ac6536ca02dc7c
Antonio Scalia Espinoza <antoniosalvatores@gmail.com>
2024-08-11 21:59:32 +0200

    acces-control-allow-headers

--- servidor.py
+++ servidor.py
@@ -16,7 +16,7 @@ estadios_partida = []
 def configure_cors_headers(response):
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type','application/json', 'Accept','application/json'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Accept'
     return response
 
 # Middleware para permitir CORS
